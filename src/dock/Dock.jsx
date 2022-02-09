3b9999b075f583e2265a1fc1eb4a9e3a137b9291
ze-castro <jmanelrcastro@hotmail.com>
2022-02-09 13:36:13 +0000

    dock recycle bin update

--- src/dock/Dock.jsx
+++ src/dock/Dock.jsx
@@ -6,37 +6,38 @@ import { useState } from "react";
 
 function Dock() {
     const [ menuBin, setMenuBin ] = useState("hidden")
-    const [ name, setName ] = useState("visable")
-
-    let num = 0
+    const [ num, setNum ] = useState(0)
 
     function emptyBin(){
         setMenuBin("visible")
-        num++
+        setNum(num+1)
+        console.log(num)
     }
 
     function hoverRestore() {
         setMenuBin("hidden")
-        num++
+        setNum(num+1)
+        console.log(num)
     }
+        
 
     return (  
         <footer className="Dock">
             <div className="dock-container">
                 <li className="li-1">
-                    <div className="name" style={{visibility: name}}>Finder</div>
+                    <div className="name">Finder</div>
                     <img className="ico" src="https://uploads-ssl.webflow.com/5f7081c044fb7b3321ac260e/5f70853981255cc36b3a37af_finder.png" alt="" />
                 </li>
                 <li className="li-2">
-                    <div className="name" style={{visibility: name}}>Siri</div>
+                    <div className="name">Siri</div>
                     <img className="ico" src="https://uploads-ssl.webflow.com/5f7081c044fb7b3321ac260e/5f70853ff3bafbac60495771_siri.png" alt="" />
                 </li>
                 <li className="li-3">
-                    <div className="name" style={{visibility: name}}>LaunchPad</div>
+                    <div className="name">LaunchPad</div>
                     <img className="ico" src="https://uploads-ssl.webflow.com/5f7081c044fb7b3321ac260e/5f70853943597517f128b9b4_launchpad.png" alt="" />
-                </li> 
-                <li className="li-bin" onClick={num / 2 == 0  ? emptyBin : hoverRestore}>
-                    <div className="name" style={{visibility: name}}>Bin</div>
+                </li>
+                <li className="li-bin" onClick={num % 2 == 0 ? emptyBin : hoverRestore()}>
+                    <div className="name">Bin</div>
                     <img className="ico" src={binFull} alt="bin" />
                     <div className="menu-bin" style={{visibility: menuBin}}>
                         <p>Fill</p>

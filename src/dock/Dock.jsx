86bc54a7170b1cbd54e036295a2d3901f94c939b
ze-castro <jmanelrcastro@hotmail.com>
2022-02-11 02:35:42 +0000

    dock animation done

--- src/dock/Dock.jsx
+++ src/dock/Dock.jsx
@@ -14,52 +14,57 @@ function Dock() {
     const [ name, setName ] = useState("")
     const [ num, setNum ] = useState(0)
     const [ src, setSrc] = useState(binFull)
-    const [ open, setOpen] = useState("")
-    const [ bounce, setBounce] = useState("")
 
+    //show context menu
     function emptyBin(){
         setMenuBin("visible")
         setName("hidden")
         setNum(num+1)
     }
 
+    //hide context menu
     function hoverRestore() {
         setMenuBin("hidden")
         setName("")
         setNum(num+1)
     }
 
+    //change bin icon for clean
     function fill(){
         setSrc(binFull)
     }
 
+    //change bin icon for full
     function empty(){
         setSrc(bin)
     }
 
-    function clicked(){
-        let li1 = document.getElementsByClassName("li-1")[0]
-        let ico = document.getElementsByClassName("ico")[0]
-        li1.classList.add("opened")
+    //dot and animation for icons in dock
+    function clicked(num){
+        let shortcut = document.getElementsByClassName("shortcut")[num]
+        let ico = document.getElementsByClassName("ico")[num]
         ico.classList.add("bounce") 
+        setTimeout(() => {
+            shortcut.classList.add("opened")
+        }, 1400);
     }
         
     return (  
         <footer className="Dock">
             <div className="dock-container">
-                <li className="li-1" onClick={clicked}>
+                <li className="li-1 shortcut" onClick={()=>{clicked(0)}}>
                     <div className="name">Mail</div>
                     <img className="ico" src={mail} alt="" />
                 </li>
-                <li className="li-2" onClick={clicked}>
+                <li className="li-2 shortcut" onClick={()=>{clicked(1)}}>
                     <div className="name">Terminal</div>
                     <img className="ico" src={terminal} alt="" />
                 </li>
-                <li className="li-3" onClick={clicked}>
+                <li className="li-3 shortcut" onClick={()=>{clicked(2)}}>
                     <div className="name">Music</div>
                     <img className="ico" src={music} alt="" />
                 </li>
-                <li className="li-4" onClick={clicked}>
+                <li className="li-4 shortcut" onClick={()=>{clicked(3)}}>
                     <div className="name">Photos</div>
                     <img className="ico" src={photos} alt="" />
                 </li>
